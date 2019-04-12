5f46664847e74949b477dd89d88a9dcf7c20a98b
MindChirp <33499052+MindChirp@users.noreply.github.com>
2019-04-12 10:12:18 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//Get the color on startup
+
+var retColor = JSON.parse(localStorage.getItem("color"));
+var els = document.getElementsByName("colored");
+console.log(els);
+setTimeout(function() {
+ els.forEach(function(e) {
+    e.setAttribute("style", "background-color: "+ retColor + ";");
+    document.getElementById("color-picker").setAttribute("value", retColor);
+})   
+}, 1)
+
+
 
 function debuggingTool() {
     this.write = function(txt) {
@@ -16,9 +29,16 @@ function debuggingTool() {
             if(l % 2 == 0) {
               //  p.setAttribute("style", "background-color: rgb(230,230,230);");
             } else {
-                p.setAttribute("style", "background-color: rgb(240,240,240);")
-            }
-         }
+                var col = document.getElementById("color-picker").value;
+
+                p.setAttribute("style", "background-color:" + col + ";");
+                p.setAttribute("name", "colored");
+
+
+
+
+                  }
+                }
 
 
 
@@ -33,4 +53,19 @@ function debuggingTool() {
 const teller = new debuggingTool();
 
 
+
+document.getElementById("color-picker").addEventListener("input", function() {
+    var cols = document.getElementsByName("colored");
+    var col = document.getElementById("color-picker").value;
+    cols.forEach(function(e) {
+        e.setAttribute("style", "background-color: " + col + ";");
+    })
+
+
+
+    //Write to memory
+
+    localStorage.setItem("color", JSON.stringify(col));
+
+})
 
